f4ab2e11f56911ce85d24220b132e6eb97e2b183
Jeremy <Jeremy@Jeremy-PC>
2015-01-20 21:11:15 -0500

    Added Slots

---
+++ Exam/src/SlotMachine.java
@@ -0,0 +1,9 @@
+public class SlotMachine {
+    private int payoutcycle, payoutamount, timetillnextpay;
+    
+    public SlotMachine(int pc, int pa, int tp){
+        payoutcycle = pc;
+        payoutamount = pa;
+        timetillnextpay = tp;
+    }
+}
